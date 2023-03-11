0813b62484d079eb1f906d2d8d18360d8442ac5a
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2023-03-11 14:55:51 +0900

    Initial Commit

---
+++ Project9/ContentView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  Project9
+//
+//  Created by 山本響 on 2023/03/11.
+//
+
+import SwiftUI
+
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        
+        return path
+    }
+}
+
+struct Arc: Shape {
+    let startAngle: Angle
+    let endAngle: Angle
+    let clockwise: Bool
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.addArc(center: CGPoint(x: rect.minX, y: rect.minY), radius: rect.width / 2, startAngle: startAngle, endAngle: startAngle, clockwise: clockwise)
+        
+        return path
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        Triangle()
+            .stroke(.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+            .frame(width: 300, height: 300)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project9/Project9App.swift
@@ -0,0 +1,17 @@
+//
+//  Project9App.swift
+//  Project9
+//
+//  Created by 山本響 on 2023/03/11.
+//
+
+import SwiftUI
+
+@main
+struct Project9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
